fe6a989d87e9e9b095c667f8969c280ea90ed9bd
Michael Aichmüller <m.aichmueller@gmail.com>
2023-10-28 19:28:32 +0200

    Update FindNumPy.cmake

--- cmake/FindNumPy.cmake
+++ cmake/FindNumPy.cmake
@@ -40,26 +40,26 @@
 
 # Finding NumPy involves calling the Python interpreter
 if(NumPy_FIND_REQUIRED)
-    find_package(PythonInterp REQUIRED)
+    find_package(Python3 REQUIRED COMPONENTS Interpreter)
 else()
-    find_package(PythonInterp)
+    find_package(Python3 COMPONENTS Interpreter)
 endif()
 
 if(NOT PYTHONINTERP_FOUND)
     set(NUMPY_FOUND FALSE)
 endif()
 
-execute_process(COMMAND "${PYTHON_EXECUTABLE}" "-c"
-    "import numpy as n; print(n.__version__); print(n.get_include());"
-    RESULT_VARIABLE _NUMPY_SEARCH_SUCCESS
-    OUTPUT_VARIABLE _NUMPY_VALUES
-    ERROR_VARIABLE _NUMPY_ERROR_VALUE
-    OUTPUT_STRIP_TRAILING_WHITESPACE)
+execute_process(COMMAND "${Python3_EXECUTABLE}" "-c"
+        "import numpy as n; print(n.__version__); print(n.get_include());"
+        RESULT_VARIABLE _NUMPY_SEARCH_SUCCESS
+        OUTPUT_VARIABLE _NUMPY_VALUES
+        ERROR_VARIABLE _NUMPY_ERROR_VALUE
+        OUTPUT_STRIP_TRAILING_WHITESPACE)
 
 if(NOT _NUMPY_SEARCH_SUCCESS MATCHES 0)
     if(NumPy_FIND_REQUIRED)
         message(FATAL_ERROR
-            "NumPy import failure:\n${_NUMPY_ERROR_VALUE}")
+                "NumPy import failure:\n${_NUMPY_ERROR_VALUE}")
     endif()
     set(NUMPY_FOUND FALSE)
 endif()
@@ -80,10 +80,10 @@ list(GET _NUMPY_VERSION_LIST 1 NUMPY_VERSION_MINOR)
 list(GET _NUMPY_VERSION_LIST 2 NUMPY_VERSION_PATCH)
 string(REGEX MATCH "[0-9]*" NUMPY_VERSION_PATCH ${NUMPY_VERSION_PATCH})
 math(EXPR NUMPY_VERSION_DECIMAL
-    "(${NUMPY_VERSION_MAJOR} * 10000) + (${NUMPY_VERSION_MINOR} * 100) + ${NUMPY_VERSION_PATCH}")
+        "(${NUMPY_VERSION_MAJOR} * 10000) + (${NUMPY_VERSION_MINOR} * 100) + ${NUMPY_VERSION_PATCH}")
 
 find_package_message(NUMPY
-    "Found NumPy: version \"${NUMPY_VERSION}\" ${NUMPY_INCLUDE_DIRS}"
-    "${NUMPY_INCLUDE_DIRS}${NUMPY_VERSION}")
+        "Found NumPy: version \"${NUMPY_VERSION}\" ${NUMPY_INCLUDE_DIRS}"
+        "${NUMPY_INCLUDE_DIRS}${NUMPY_VERSION}")
 
-set(NUMPY_FOUND TRUE)
+set(NumPy_FOUND TRUE)
