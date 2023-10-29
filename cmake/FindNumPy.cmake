7d1e47156dcdb4853dac8a0f6a6ddb6d44c49294
Michael Aichmüller <m.aichmueller@gmail.com>
2023-10-29 13:37:22 +0100

    Update FindNumPy.cmake

--- cmake/FindNumPy.cmake
+++ cmake/FindNumPy.cmake
@@ -38,52 +38,60 @@
 # 
 #============================================================================
 
-# Finding NumPy involves calling the Python interpreter
-if(NumPy_FIND_REQUIRED)
-    find_package(Python3 REQUIRED COMPONENTS Interpreter)
+if(${Python3_NumPy_FOUND})
+    set(NUMPY_INCLUDE_DIRS "${Python3_NumPy_FOUND}")
+    set(NUMPY_FOUND TRUE)
+elif(${NumPy_FOUND})
+    set(NUMPY_INCLUDE_DIRS "${NumPy_INCLUDE_DIRS}")
+    set(NUMPY_FOUND TRUE)
 else()
-    find_package(Python3 COMPONENTS Interpreter)
-endif()
-
-if(NOT PYTHONINTERP_FOUND)
-    set(NUMPY_FOUND FALSE)
-endif()
-
-execute_process(COMMAND "${Python3_EXECUTABLE}" "-c"
-        "import numpy as n; print(n.__version__); print(n.get_include());"
-        RESULT_VARIABLE _NUMPY_SEARCH_SUCCESS
-        OUTPUT_VARIABLE _NUMPY_VALUES
-        ERROR_VARIABLE _NUMPY_ERROR_VALUE
-        OUTPUT_STRIP_TRAILING_WHITESPACE)
-
-if(NOT _NUMPY_SEARCH_SUCCESS MATCHES 0)
+    # Finding NumPy involves calling the Python interpreter
     if(NumPy_FIND_REQUIRED)
-        message(FATAL_ERROR
-                "NumPy import failure:\n${_NUMPY_ERROR_VALUE}")
+        find_package(Python3 REQUIRED COMPONENTS Interpreter)
+    else()
+        find_package(Python3 COMPONENTS Interpreter)
     endif()
-    set(NUMPY_FOUND FALSE)
+    
+    if(NOT PYTHONINTERP_FOUND)
+        set(NUMPY_FOUND FALSE)
+    endif()
+    
+    execute_process(COMMAND "${Python3_EXECUTABLE}" "-c"
+            "import numpy as n; print(n.__version__); print(n.get_include());"
+            RESULT_VARIABLE _NUMPY_SEARCH_SUCCESS
+            OUTPUT_VARIABLE _NUMPY_VALUES
+            ERROR_VARIABLE _NUMPY_ERROR_VALUE
+            OUTPUT_STRIP_TRAILING_WHITESPACE)
+    
+    if(NOT _NUMPY_SEARCH_SUCCESS MATCHES 0)
+        if(NumPy_FIND_REQUIRED)
+            message(FATAL_ERROR
+                    "NumPy import failure:\n${_NUMPY_ERROR_VALUE}")
+        endif()
+        set(NUMPY_FOUND FALSE)
+    endif()
+    
+    # Convert the process output into a list
+    string(REGEX REPLACE ";" "\\\\;" _NUMPY_VALUES ${_NUMPY_VALUES})
+    string(REGEX REPLACE "\n" ";" _NUMPY_VALUES ${_NUMPY_VALUES})
+    list(GET _NUMPY_VALUES 0 NUMPY_VERSION)
+    list(GET _NUMPY_VALUES 1 NUMPY_INCLUDE_DIRS)
+    
+    # Make sure all directory separators are '/'
+    string(REGEX REPLACE "\\\\" "/" NUMPY_INCLUDE_DIRS ${NUMPY_INCLUDE_DIRS})
+    
+    # Get the major and minor version numbers
+    string(REGEX REPLACE "\\." ";" _NUMPY_VERSION_LIST ${NUMPY_VERSION})
+    list(GET _NUMPY_VERSION_LIST 0 NUMPY_VERSION_MAJOR)
+    list(GET _NUMPY_VERSION_LIST 1 NUMPY_VERSION_MINOR)
+    list(GET _NUMPY_VERSION_LIST 2 NUMPY_VERSION_PATCH)
+    string(REGEX MATCH "[0-9]*" NUMPY_VERSION_PATCH ${NUMPY_VERSION_PATCH})
+    math(EXPR NUMPY_VERSION_DECIMAL
+            "(${NUMPY_VERSION_MAJOR} * 10000) + (${NUMPY_VERSION_MINOR} * 100) + ${NUMPY_VERSION_PATCH}")
+    
+    find_package_message(NUMPY
+            "Found NumPy: version \"${NUMPY_VERSION}\" ${NUMPY_INCLUDE_DIRS}"
+            "${NUMPY_INCLUDE_DIRS}${NUMPY_VERSION}")
+    
+    set(NumPy_FOUND TRUE)
 endif()
-
-# Convert the process output into a list
-string(REGEX REPLACE ";" "\\\\;" _NUMPY_VALUES ${_NUMPY_VALUES})
-string(REGEX REPLACE "\n" ";" _NUMPY_VALUES ${_NUMPY_VALUES})
-list(GET _NUMPY_VALUES 0 NUMPY_VERSION)
-list(GET _NUMPY_VALUES 1 NUMPY_INCLUDE_DIRS)
-
-# Make sure all directory separators are '/'
-string(REGEX REPLACE "\\\\" "/" NUMPY_INCLUDE_DIRS ${NUMPY_INCLUDE_DIRS})
-
-# Get the major and minor version numbers
-string(REGEX REPLACE "\\." ";" _NUMPY_VERSION_LIST ${NUMPY_VERSION})
-list(GET _NUMPY_VERSION_LIST 0 NUMPY_VERSION_MAJOR)
-list(GET _NUMPY_VERSION_LIST 1 NUMPY_VERSION_MINOR)
-list(GET _NUMPY_VERSION_LIST 2 NUMPY_VERSION_PATCH)
-string(REGEX MATCH "[0-9]*" NUMPY_VERSION_PATCH ${NUMPY_VERSION_PATCH})
-math(EXPR NUMPY_VERSION_DECIMAL
-        "(${NUMPY_VERSION_MAJOR} * 10000) + (${NUMPY_VERSION_MINOR} * 100) + ${NUMPY_VERSION_PATCH}")
-
-find_package_message(NUMPY
-        "Found NumPy: version \"${NUMPY_VERSION}\" ${NUMPY_INCLUDE_DIRS}"
-        "${NUMPY_INCLUDE_DIRS}${NUMPY_VERSION}")
-
-set(NumPy_FOUND TRUE)
